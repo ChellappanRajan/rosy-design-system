af6fb99b5940dac28c9a3d841337ac8ed5359404
ChellappanRajan <vrchellappan@gmail.com>
2024-08-25 19:38:26 +0530

    docs(shimmer): add js documentation for loader component

--- libs/rosy-design-system/src/lib/shimmer-loader/shimmer-loader.ts
+++ libs/rosy-design-system/src/lib/shimmer-loader/shimmer-loader.ts
@@ -6,7 +6,44 @@ import {styles} from './shimmer';
 export class RosyShimmerLoader extends LitElement {
   static override styles = styles;
 
-  @property({ reflect:true, type:String }) accessor shape :'rounded' | 'rounded-md' | 'rounded-lg' | 'rounded-full' | 'rounded-none' = 'rounded-none';
+/**
+ * The `shape` property controls the border radius styling of the shimmer loader component.
+ *
+ * @property {string} shape
+ * @default 'rounded-none'
+ * @reflects true
+ * @type {'rounded' | 'rounded-md' | 'rounded-lg' | 'rounded-full' | 'rounded-none'}
+ *
+ * @description
+ * This property determines the border radius applied to the component's corners. The possible values are:
+ * - `'rounded'`: Small border-radius applied to the corners.
+ * - `'rounded-md'`: Medium border-radius applied to the corners.
+ * - `'rounded-lg'`: Large border-radius applied to the corners.
+ * - `'rounded-full'`: Maximum border-radius, making the corners fully rounded (circular or oval).
+ * - `'rounded-none'`: No border-radius, keeping the corners sharp.
+ *
+ * The `shape` property reflects its value to a corresponding HTML attribute, allowing for CSS-based styling. The default value is `'rounded-none'`.
+ *
+ * @example
+ * // Usage in HTML
+ * <rosy-shimmer-loader shape="rounded-lg"></rosy-shimmer-loader>
+ *
+ * // JavaScript example
+ * const component = document.querySelector('rosy-shimmer-loader');
+ * component.shape = 'rounded-full';
+ *
+ * @example
+ * // JSX example
+ * import React from 'react';
+ *
+ * function App() {
+ *  return (
+ *     <RosyShimmerLoader shape="rounded-md" />
+ *   );
+ * }
+ *
+ */
+   @property({ reflect:true, type:String }) accessor shape :'rounded' | 'rounded-md' | 'rounded-lg' | 'rounded-full' | 'rounded-none' = 'rounded-none';
 
 
   override render() {
