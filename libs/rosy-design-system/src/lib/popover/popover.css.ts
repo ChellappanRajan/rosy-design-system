66a42f822991708fa4a84345d4a468b4e255d3be
ChellappanRajan <vrchellappan@gmail.com>
2024-09-08 11:44:28 +0530

    feat(popover): add new position property
    - Introduced the `position` property to control the alignment of the popover relative to its anchor element.
    - Supports 'center-left', 'top', and 'bottom' values with a default of 'center-right'.

--- libs/rosy-design-system/src/lib/popover/popover.css.ts
+++ libs/rosy-design-system/src/lib/popover/popover.css.ts
@@ -1,67 +1,107 @@
-import {css} from 'lit';
-
+import { css } from 'lit';
 
 export const styles = css`
-.btn{
-  width: fit-content;
+  .btn {
+    width: fit-content;
     border-radius: 1rem;
     border: none;
-    box-shadow: 0 4px 8px 3px rgba(0, 0, 0, .15), 0 1px 3px rgba(0, 0, 0, .3);
+    box-shadow: 0 4px 8px 3px rgba(0, 0, 0, 0.15), 0 1px 3px rgba(0, 0, 0, 0.3);
     background: white;
     padding: 0.25rem 1rem;
     anchor-name: --popover-trigger-element;
-}
-svg{
+  }
+  svg {
     width: 20px;
     height: 20px;
-}
-
-
-:popover-open {
-  box-shadow: 0 2px 6px 2px rgba(0, 0, 0, .15), 0 1px 2px 0 rgba(0, 0, 0, .3);
-  margin:0;
-  height:100px;
-  background-color:#fff;
-  display: flex;
-  max-width: 200px;
-  padding: 1rem;
-  height:100px;
-  border:none;
-  inset:auto;
-  position:absolute;
-  flex-direction:column;
-  gap:0.5rem;
-  border-radius:10px;
-  position-anchor:--popover-trigger-element;
-}
-:host([position="center-right"]){
-  & :popover-open{
-  top: anchor(--popover-trigger-element center);
-  left: anchor(--popover-trigger-element right);
   }
 
-}
-.header{
-  display: flex;
+  [tabindex='-1'] {
+    outline: none;
+  }
+  :popover-open {
+    box-shadow: 0 2px 6px 2px rgba(0, 0, 0, 0.15),
+      0 1px 2px 0 rgba(0, 0, 0, 0.3);
+    margin: 0;
+    height: 100px;
+    background-color: #fff;
+    display: flex;
+    max-width: 200px;
+    padding: 1rem;
+    height: 100px;
+    border: none;
+    /*Since popover attribute by default have inset:0 without these positioing not working
+  */
+    inset: auto;
+    position: absolute;
+    flex-direction: column;
+    gap: 0.5rem;
+    border-radius: 10px;
+    position-anchor: --popover-trigger-element;
+  }
+
+  :host([position='center-left']) {
+    & :popover-open {
+      inset-block-start: anchor(--popover-trigger-element center);
+      inset-inline-end: anchor(--popover-trigger-element left);
+    }
+  }
+
+  :host([position='top']) {
+    & :popover-open {
+      inset-area: block-top;
+    }
+  }
+  :host([position='bottom']) {
+    & :popover-open {
+      inset-area: block-end;
+    }
+  }
+
+  :host([position='center-right']) {
+    & :popover-open {
+      /* top of the popover container - anchor center
+  *block-top/bottom
+  *inline-left/righ
+  *position-area should try
+  * */
+
+      inset-block-start: anchor(--popover-trigger-element center);
+
+      inset-inline-start: anchor(--popover-trigger-element right);
+
+      /* position-try-fallbacks:flip-block flip-inline;
+    */
+    }
+  }
+
+  // @position-try --bottom{
+  //   bottom:anchor(--popover-trigger-element center);
+  // }
+
+  .header {
+    display: flex;
     justify-content: space-between;
     align-items: center;
-    // padding:0.5rem;
-}
-h2{
-  margin:0;
-}
-main{
-}
-.close-button{
+  }
+  h2 {
+    margin: 0;
+  }
+  main {
+  }
+  .close-button {
     border: none;
     padding: 0.25rem;
     border-radius: 50%;
     display: flex;
     align-items: center;
     justify-content: center;
-}
-p{
-margin:0;
-}
+    border: 2px solid transparent;
+    cursor: pointer;
+  }
+  .close-button:hover {
+    border: 2px solid #ffde4d;
+  }
+  p {
+    margin: 0;
+  }
 `;
-
--- libs/rosy-design-system/src/lib/popover/popover.ts
+++ libs/rosy-design-system/src/lib/popover/popover.ts
@@ -1,42 +1,63 @@
-
-import {html,LitElement, nothing} from 'lit';
-import {customElement, property,state, queryAssignedElements, queryAssignedNodes} from 'lit/decorators.js';
-import {styles} from './popover.css';
+import { html, LitElement, nothing } from 'lit';
+import {
+  customElement,
+  property,
+  query,
+  queryAssignedElements,
+  state,
+} from 'lit/decorators.js';
+import { styles } from './popover.css';
 
 @customElement('rosy-popover')
 export class Popover extends LitElement {
   static override styles = styles;
 
-  // static override shadowRootOptions = {...LitElement.shadowRootOptions, delegatesFocus: true};
+  /**
+   * The position of the popover relative to its anchor element.
+   *
+   * @property {string} position - The position of the popover.
+   * Possible values are:
+   * - 'center-right': Aligns the popover to the center of the right side of the anchor.
+   * - 'center-left': Aligns the popover to the center of the left side of the anchor.
+   * - 'top': Aligns the popover above the anchor.
+   * - 'bottom': Aligns the popover below the anchor.
+   *
+   * @default 'center-left'
+   * @reflects
+   */
+  @property({ reflect: true, type: String }) accessor position:
+    | 'center-right'
+    | 'center-left'
+    | 'top'
+    | 'bottom' = 'center-right';
 
-
-  @property({reflect:true,type:String}) accessor position = 'center-right';
-
-  @property({type:String}) override accessor title!:string;
+  @property({ type: String }) override accessor title!: string;
 
   @state() accessor isPressed = false;
-
-  // @property({attribute:false,type:String}) accessor ;
 
   @queryAssignedElements({ slot: 'trigger' })
   trigger!: Array<HTMLElement>;
 
-  #restoreFocus:Element | null = null;
+  @query('[popover]') accessor popoverElement!: HTMLElement;
 
+  #restoreFocus: Element | null = null;
 
-  private onClose(){
-    this.shadowRoot?.querySelector<HTMLElement>('[popover]')?.hidePopover();
+  #onClose() {
+    this.popoverElement.hidePopover();
   }
 
-  private onBeforeToggle(event:ToggleEvent){
-    if(event.oldState === "closed"){
+  #onToggle(event: ToggleEvent) {
+    if (event.newState === 'open') {
+      this.popoverElement.focus();
+    }
+  }
+
+  #onBeforeToggle(event: ToggleEvent) {
+    if (event.oldState === 'closed') {
       this.isPressed = true;
-      setTimeout(()=>{
-      this.shadowRoot?.querySelector<HTMLElement>('[popover]')!.focus();
-      });
       this.#restoreFocus = document.activeElement!.shadowRoot!.activeElement;
     }
-    if (event.newState === "closed") {
+    if (event.newState === 'closed') {
       this.isPressed = false;
       (this.#restoreFocus as HTMLElement)?.focus();
     }
@@ -45,32 +66,51 @@ export class Popover extends LitElement {
   override render() {
     return html`
       <div>
-      <slot @slotchange=${(e:any)=>{
-      console.log('Slot Change',e.target.querySelector('button'));
-      }} name="trigger">
-        <button popovertarget="popover" aria-controls=${this.isPressed ? 'popover' : nothing} class="btn">show popover ⓘ </button>
-      </slot>
+        <slot name="trigger">
+          <button
+            popovertarget="popover"
+            aria-controls=${this.isPressed ? 'popover' : nothing}
+            class="btn"
+          >
+            show popover ⓘ
+          </button>
+        </slot>
 
-      <section @beforetoggle=${this.onBeforeToggle} role="dialog" tabindex="-1" aria-labelledby="heading" class="popover" popover id="popover">
+        <section
+          @toggle=${this.#onToggle}
+          @beforetoggle=${this.#onBeforeToggle}
+          role="dialog"
+          tabindex="-1"
+          aria-labelledby="heading"
+          class="popover"
+          popover
+          id="popover"
+        >
           <div class="header">
             <slot name="title">
               <h2 id="heading">${this.title}</h2>
             </slot>
-            <button @click=${this.onClose} class="close-button">
-<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+            <button @click=${this.#onClose} class="close-button">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#5f6368"
+              >
+                <path
+                  d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
+                />
+              </svg>
             </button>
           </div>
           <main>
-          <slot name="body">
-          <p>
-              Greetings, one and all!
-          </p>
-          </slot>
+            <slot name="body">
+              <p>Greetings, one and all!</p>
+            </slot>
           </main>
           <div class="footer">
-          <slot name="footer">
-            Hello footer
-          </slot>
+            <slot name="footer"> Hello footer </slot>
           </div>
         </section>
       </div>
