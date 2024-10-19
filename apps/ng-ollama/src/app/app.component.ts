420f9e96df46a583cf67f872d58032ecc3c7fac6
ChellappanRajan <vrchellappan@gmail.com>
2024-10-19 13:20:19 +0530

    chore: read stream data from API

--- apps/ng-ollama/src/app/app.component.ts
+++ apps/ng-ollama/src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, computed, effect, inject, signal, ViewEncapsulation } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { response } from 'express';
 
+
+const URL = `http://localhost:8080/stream`;
 @Component({
   standalone: true,
   imports: [RouterModule],
@@ -8,4 +12,49 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+
+
+
+response = signal('');
+
+_ = effect(()=>{
+  console.log(`[Log] ${this.response()}`);
+})
+
+
+
+
+
+fetchData() {
+  fetch(URL).then((response) => {
+    const reader = response.body!.getReader();
+    const  _this  = this;
+    return new ReadableStream<Uint8Array>({
+      start(controller) {
+        return pump();
+        function pump(): any {
+          return reader.read().then(({ done, value }) => {
+            // When no more data needs to be consumed, close the stream
+            if (done) {
+              console.info('[Stream] Completed...')
+              controller.close();
+              return;
+            }
+
+            const chunk = new TextDecoder("utf-8").decode(value);
+            const oldResponse = _this.response();
+            _this.response.set(`${oldResponse} ${chunk}`);
+            // Enqueue the next data chunk into our target stream
+            controller.enqueue(value);
+            return pump();
+          });
+        }
+      },
+    });
+  }) 
+
+}
+ 
+
+}
