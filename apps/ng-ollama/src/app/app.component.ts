374b263b59fd179b4028daedca16690b9ac44e97
ChellappanRajan <vrchellappan@gmail.com>
2024-12-07 16:11:24 +0530

    chore: consume ollama platform api

--- apps/ng-ollama/src/app/app.component.ts
+++ apps/ng-ollama/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, computed, effect,ElementRef, inject, signal, viewChild, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  ElementRef,
+  inject,
+  signal,
+  viewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-
-
-
-const URL = `http://localhost:8080/stream`;
+// const URL = `http://localhost:8080/stream`;
+const URL = `http://localhost:11434/api/generate`;
 @Component({
   standalone: true,
   imports: [RouterModule],
@@ -14,64 +21,73 @@ const URL = `http://localhost:8080/stream`;
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  elementRef = viewChild('ref');
+  http = inject(HttpClient);
 
-elementRef = viewChild('ref');
-http = inject(HttpClient);
+  response = signal('');
 
-response = signal('');
+  _ = effect(() => {
+    const element = this.elementRef() as ElementRef;
 
-_ = effect(()=>{
-  const element = this.elementRef() as ElementRef;
- 
-  if(this.response() && element){
-    // eslint-disable-next-line no-self-assign
-    // element.nativeElement.scrollTop = element.nativeElement.scrollTop;
+    if (this.response() && element) {
+      console.log(this.response());
+      // eslint-disable-next-line no-self-assign
+      // element.nativeElement.scrollTop = element.nativeElement.scrollTop;
+    }
+    // console.log(element.nativeElement.scrollTop);
+  });
+
+  fetchData2() {
+    this.http
+      .post(URL, {
+        model: 'llama3.2',
+        prompt: 'Why is the sky blue?',
+      })
+      .subscribe(value => {
+        console.log(value);
+      });
   }
-    console.log(element.nativeElement.scrollTop);
-})
 
-
-
-
-fetchData2(){
-    this.http.post('http://localhost:8080/api/conversation',{
-      "model": "llama3.2",
-  "prompt": "Why is the sky blue?"
-    }).subscribe((value)=>{
-       console.log(value);
-     })
-}
-
-
-fetchData() {
-  fetch(URL).then((response) => {
-    const reader = response.body!.getReader();
-    const  _this  = this;
-    return new ReadableStream<Uint8Array>({
-      start(controller) {
-        return pump();
-        function pump(): any {
-          return reader.read().then(({ done, value }) => {
-            // When no more data needs to be consumed, close the stream
-            if (done) {
-              console.info('[Stream] Completed...')
-              controller.close();
-              return;
-            }
-
-            const chunk = new TextDecoder("utf-8").decode(value);
-            const oldResponse = _this.response();
-            _this.response.set(`${oldResponse} ${chunk}`);
-            // Enqueue the next data chunk into our target stream
-            controller.enqueue(value);
-            return pump();
-          });
-        }
-      },
+  fetchData(elementRef: HTMLInputElement) {
+    const value = elementRef.value;
+    const serialization = JSON.stringify({
+      model: 'llama3.2',
+      prompt: value,
     });
-  }) 
 
-}
- 
+    fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: serialization,
+    }).then(response => {
+      const reader = response.body!.getReader();
 
+      return new ReadableStream<Uint8Array>({
+        start: controller => {
+          const push: () => void = () => {
+            return reader.read().then(({ done, value }) => {
+              // When no more data needs to be consumed, close the stream
+              if (done) {
+                console.info('[Stream] Completed...');
+                elementRef.value = '';
+                controller.close();
+                return;
+              }
+
+              const chunk = new TextDecoder('utf-8').decode(value);
+              const parseJSON = JSON.parse(chunk).response;
+              const oldResponse = this.response();
+              this.response.set(`${oldResponse} ${parseJSON}`);
+              // Enqueue the next data chunk into our target stream
+              controller.enqueue(value);
+              return push();
+            });
+          };
+          return push();
+        },
+      });
+    });
+  }
 }
