03e5fea520c0262f2cebd4a17ce3e219551cd270
ChellappanRajan <vrchellappan@gmail.com>
2025-03-07 13:59:25 +0530

    feat: enhance header styling and add scroll-driven animation documentation

--- apps/rosy-design-system/src/app/app.config.ts
+++ apps/rosy-design-system/src/app/app.config.ts
@@ -1,10 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import {
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideHttpClient(
+      withInterceptors([
+        (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+          return next(req);
+        },
+      ])
+    ),
   ],
 };
---
+++ apps/rosy-design-system/src/app/docs/ScrollDrivenAnimation.md
@@ -0,0 +1,6 @@
+By default animation based on document timeline:document.timeline.currentTime
+
+There are two types of scroll driven animation
+
+Scroll Progress Timeline - linked to scroll position of scroll container
+View Progress Timeline -  relative position of a particular element within its scroll container.
