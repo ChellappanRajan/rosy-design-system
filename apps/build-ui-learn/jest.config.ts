c7498cb36849bef9c975814fecd58e792b0f9ca1
ChellappanRajan <vrchellappan@gmail.com>
2025-05-28 20:07:52 +0530

    chore: update dependencies in package.json
    - Added "next" version 15.2.4
    - Updated "@nx/js" to version 21.1.2 and added "@nx/next" version ^21.1.2
    - Updated "@swc/cli" to version ~0.6.0
    - Added "@testing-library/dom" version 10.4.0
    - Updated "autoprefixer" to version 10.4.13
    - Added "babel-jest" version ^29.7.0
    - Updated "postcss" to version 8.4.38
    - Updated "tailwindcss" to version 3.4.3

---
+++ apps/build-ui-learn/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'build-ui-learn',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/build-ui-learn',
+};
